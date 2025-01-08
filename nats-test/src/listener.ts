671eeac9c5b48ed36caa4dad0739a1fa91b60fa3
Mihir Verma <mihirv7781@gmail.com>
2025-01-09 00:12:30 +0530

    updated test nats project

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,17 +1,36 @@
-import nats from "node-nats-streaming";
-
+import nats, { Message } from "node-nats-streaming";
+import { randomBytes } from "node:crypto";
 console.clear();
 
-const client = nats.connect("trespass", "123", {
+const client = nats.connect("trespass", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222",
 });
 
 client.on("connect", () => {
   console.log("Listener connected to NATS ");
 
-  const subscription = client.subscribe("ticket:created");
+  client.on("close", () => {
+    console.log("NATS closing connection!");
+    process.exit();
+  });
 
-  subscription.on("message", (msg) => {
+  const options = client.subscriptionOptions().setManualAckMode(true);
+
+  const subscription = client.subscribe(
+    "ticket:created",
+    "orders-service-queue-group",
+    options
+  );
+
+  subscription.on("message", (msg: Message) => {
     console.log("Message received!");
+    const data = msg.getData();
+    if (typeof data === "string") {
+      console.log(` Recieved event #${msg.getSequence()} with data: ${data}`);
+    }
+    msg.ack();
   });
 });
+
+process.on("SIGINT", () => client.close());
+process.on("SIGTERM", () => client.close());
